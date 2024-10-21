a6fe6d7c241b97f27dd3288d9f4913758be0da6d
Andrea Catapano <a.catapano96@gmail.com>
2024-10-21 15:45:45 +0200

    Initial Commit

---
+++ SwiftulCrypto/SwiftulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulCryptoApp.swift
+//  SwiftulCrypto
+//
+//  Created by Andrea Catapano on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftulCryptoTests/SwiftulCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulCryptoTests.swift
+//  SwiftulCryptoTests
+//
+//  Created by Andrea Catapano on 21/10/24.
+//
+
+import Testing
+@testable import SwiftulCrypto
+
+struct SwiftulCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
