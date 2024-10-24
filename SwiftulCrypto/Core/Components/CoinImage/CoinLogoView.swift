fedaeaf3ded892bc524fba5192983caaa46d9bd3
Andrea Catapano <a.catapano96@gmail.com>
2024-10-24 10:35:11 +0200

    [Lezione] 14 creata Sheet PortofolioView

---
+++ SwiftulCrypto/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinLogoView.swift
+//  SwiftulCrypto
+//
+//  Created by Andrea Catapano on 23/10/24.
+//
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinLogoView()
+}
